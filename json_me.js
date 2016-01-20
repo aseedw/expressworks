169067e246bdcd1438cd0711ca8d49c0e5c69759
jeffcheng <b98502008@gmail.com>
2016-01-20 18:12:05 +0800

    Create json_me.js

---
+++ json_me.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.get('/books', function (req, res) {
+  fs.readFile(process.argv[3], function (err, data) {
+    if (err) {
+      throw err;
+    }
+   var object = JSON.parse(data);
+    res.send(object);
+  });
+});
+
+app.listen(process.argv[2]);
